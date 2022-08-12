732c22f31305a5cd95f02876818479477dac4985
abdelrhmannabil12 <59992853+abdelrhmannabil12@users.noreply.github.com>
2022-08-12 18:07:58 +0200

    models created and connected with templates

--- store/views.py
+++ store/views.py
@@ -1,9 +1,18 @@
+from ast import Or
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
 def store(request):
-    return render(request,'store/store.html')
+    books=Book.objects.all()
+    return render(request,'store/store.html',{'books':books})
 def profile(request):
-    return render(request,'store/profile.html')
+    if request.user.is_authenticated:
+        customer=request.user.customer
+        order , created=Order.objects.get_or_create(customer=customer,compelte=False)
+        items=order.orderitem_set.all()
+
+    else:
+        items=[]
+    return render(request,'store/profile.html',{'items':items,'order':order})
 def logout(request):
     return render(request,'store/logout.html')
