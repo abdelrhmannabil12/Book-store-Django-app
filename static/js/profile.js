6d73b21b6dc6280e2755d7a2558359cc7de65ddd
abdelrhmannabil12 <59992853+abdelrhmannabil12@users.noreply.github.com>
2022-08-14 16:54:59 +0200

    removed togglepopup function

--- static/js/profile.js
+++ static/js/profile.js
@@ -1,43 +1,41 @@
-var updatebtns=document.getElementsByClassName('update-cart')
+var updatebtns = document.getElementsByClassName('update-cart')
 
-for(var i=0;i<updatebtns.length;i++){
-    updatebtns[i].addEventListener('click',function(){
+for (var i = 0; i < updatebtns.length; i++) {
+    updatebtns[i].addEventListener('click', function () {
         var bookid = this.dataset.book
-        var action=this.dataset.action
-        console.log(bookid,action)
+        var action = this.dataset.action
+        console.log(bookid, action)
 
-        if(user === "AnonymousUser"){
+        if (user === "AnonymousUser") {
             console.log('no logged in')
         }
-        else{updateUserOrder(bookid,action)
+        else {
+            updateUserOrder(bookid, action)
         }
     })
 }
 
 
-function updateUserOrder(bookid,action){
+function updateUserOrder(bookid, action) {
     console.log("user is logged in , sending data")
 
-    var url='/update_item/'
+    var url = '/update_item/'
 
-    fetch(url,{
+    fetch(url, {
         method: 'POST',
-        headers:{
-            'Content-Type':'application/json',
-            'X-CSRFToken':csrftoken
+        headers: {
+            'Content-Type': 'application/json',
+            'X-CSRFToken': csrftoken
         },
-        body:JSON.stringify({'bookid':bookid,'action':action})
+        body: JSON.stringify({ 'bookid': bookid, 'action': action })
     })
-    .then((response)=>{
-        return response.json()
-    })
-    .then((data)=>{
-        console.log('data: ',data)
-        location.reload()
-    })
+        .then((response) => {
+            return response.json()
+        })
+        .then((data) => {
+            console.log('data: ', data)
+            location.reload()
+        })
 }
 
 
-function togglePopup(){
-    document.getElementById("popup-1").classList.toggle("active");
-}
