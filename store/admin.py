f405db932af75a2fff0e8e175cce3b1ad471a8ba
abdelrhmannabil12 <59992853+abdelrhmannabil12@users.noreply.github.com>
2022-08-14 16:55:42 +0200

    customized admin panel by adding two classes to show more data in table

--- store/admin.py
+++ store/admin.py
@@ -1,8 +1,16 @@
 from msilib.schema import AdminExecuteSequence
 from django.contrib import admin
+
+from .views import register
 from .models import *
 # Register your models here.
 admin.site.register(Customer)
-admin.site.register(Book)
-admin.site.register(Order)
 admin.site.register(OrderItem)
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ['title','author','price']
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id','itemsTotal','orderTotal']
